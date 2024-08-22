a9fc711ad74b147218189ca7867a7f4c1c8e0c18
rupankdas <rupandas@gmail.com>
2024-08-22 13:11:37 +0800

    Updates

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BTNRunCode = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.TextBoxNumber1 = new System.Windows.Forms.TextBox();
+            this.TextBoxNumber2 = new System.Windows.Forms.TextBox();
+            this.ListBoxOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BTNRunCode
+            // 
+            this.BTNRunCode.Location = new System.Drawing.Point(223, 44);
+            this.BTNRunCode.Name = "BTNRunCode";
+            this.BTNRunCode.Size = new System.Drawing.Size(219, 66);
+            this.BTNRunCode.TabIndex = 0;
+            this.BTNRunCode.Text = "Run Code";
+            this.BTNRunCode.UseVisualStyleBackColor = true;
+            this.BTNRunCode.Click += new System.EventHandler(this.BTNRunCode_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(37, 147);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(50, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Number1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(35, 186);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(50, 13);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Number2";
+            // 
+            // TextBoxNumber1
+            // 
+            this.TextBoxNumber1.Location = new System.Drawing.Point(134, 150);
+            this.TextBoxNumber1.Name = "TextBoxNumber1";
+            this.TextBoxNumber1.Size = new System.Drawing.Size(241, 20);
+            this.TextBoxNumber1.TabIndex = 3;
+            // 
+            // TextBoxNumber2
+            // 
+            this.TextBoxNumber2.Location = new System.Drawing.Point(133, 188);
+            this.TextBoxNumber2.Name = "TextBoxNumber2";
+            this.TextBoxNumber2.Size = new System.Drawing.Size(241, 20);
+            this.TextBoxNumber2.TabIndex = 4;
+            // 
+            // ListBoxOutput
+            // 
+            this.ListBoxOutput.FormattingEnabled = true;
+            this.ListBoxOutput.Location = new System.Drawing.Point(49, 243);
+            this.ListBoxOutput.Name = "ListBoxOutput";
+            this.ListBoxOutput.Size = new System.Drawing.Size(367, 134);
+            this.ListBoxOutput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListBoxOutput);
+            this.Controls.Add(this.TextBoxNumber2);
+            this.Controls.Add(this.TextBoxNumber1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.BTNRunCode);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BTNRunCode;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox TextBoxNumber1;
+        private System.Windows.Forms.TextBox TextBoxNumber2;
+        private System.Windows.Forms.ListBox ListBoxOutput;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,39 @@ namespace A2
         {
             InitializeComponent();
         }
+
+        private void BTNRunCode_Click(object sender, EventArgs e)
+        {
+            // Clear the ListBox for output 
+            ListBoxOutput.Items.Clear();
+            // Declare several variables
+             int num1 = 0;
+             int num2 = 0; 
+             int sum = 0; 
+             int product = 0;
+            // Test if both TextBoxes have integer values
+            if (!((int.TryParse(TextBoxNumber1.Text, out num1)) 
+                && (int.TryParse(TextBoxNumber2.Text, out num2)))) 
+            { // Report error message
+              MessageBox.Show("That is not an integer"); 
+            }
+            else
+            {
+                if(num1>num2)
+                {
+                    ListBoxOutput.Items.Add("Number 1 is greater");
+                }
+                else
+                {
+                    ListBoxOutput.Items.Add("Number 2 is greater");
+                }
+                sum = num1 + num2; 
+                product = num1 * num2; 
+                ListBoxOutput.Items.Add("The sum is " + sum); 
+                ListBoxOutput.Items.Add("The product is " + product);
+            }
+            TextBoxNumber1.Clear(); 
+            TextBoxNumber2.Clear();
+        }
     }
 }
